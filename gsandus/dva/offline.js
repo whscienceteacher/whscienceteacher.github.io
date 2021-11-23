c6b60765053378fd1ef9989abd84d862432eae99
Wild Haird Science Teacher <69084880+whscienceteacher@users.noreply.github.com>
2021-11-22 19:40:30 -0600

    gsandus dva 0.1.4.0

---
+++ gsandus/dva/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1637631396,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/physobjtemplate-sheet0.png",
+		"images/ink-sheet0.png",
+		"images/slider-sheet0.png",
+		"images/sliderfill-sheet0.png",
+		"images/target-sheet0.png",
+		"images/hiddenbutton1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff",
+		"unknowns.csv",
+		"givens.csv"
+	]
+}

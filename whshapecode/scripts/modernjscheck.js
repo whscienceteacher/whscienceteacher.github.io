9512aab5f6550a486d5e3ff2b033d382c88996fd
whscienceteacher <69084880+whscienceteacher@users.noreply.github.com>
2025-09-06 12:09:21 -0500

    moved orbit simulations for folder parsimony

--- whshapecode/scripts/modernjscheck.js
+++ whshapecode/scripts/modernjscheck.js
@@ -1,1 +1,1 @@
-"use strict";!function(){let t=!0;t&&(window["C3_ModernJSSupport_OK"]=!0)}();
+"use strict";!function(){let p=!0;"pass"!==(new class{#p="pass";getProp(){return this.#p}}).getProp()&&(p=!1),p&&(window["C3_ModernJSSupport_OK"]=!0)}();
